f63070e1ba5511b02d4a3df3181132eaa33b43af
Daniel Brockman <daniel@gointeractive.se>
2010-08-06 19:57:38 +0200

    Add GPL 3.

--- lib/asautotest/utilities.rb
+++ lib/asautotest/utilities.rb
@@ -1,3 +1,22 @@
+# -*- coding: utf-8 -*-
+# utilities.rb --- various handy utilities and monkey patches
+# Copyright (C) 2010  Go Interactive
+
+# This file is part of asautotest.
+
+# asautotest is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# asautotest is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with asautotest.  If not, see <http://www.gnu.org/licenses/>.
+
 module Kernel
   def returning(value)
     yield value ; value
